3aed940c22129bd5a52bb2777ecf94798301f690
DESKTOP-F9A0BAC\Jhyoon <jhyoon@gabia.com>
2018-12-11 13:32:00 +0900

    Parallex Init

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+7
+});
+
+$(".main").onepage_scroll({
+	sectionContainer: "section",
+	easing: "ease",
+	animationTime: 1000,
+	pagination: true,
+	updateURL: false,
+	beforeMove: function(index) {},
+	loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+	keyboard: true,                  // You can activate the keyboard controls
+	responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+	direction: "vertical"
+});
